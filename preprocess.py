c588fd91e2e34fd8fa5b43625f209cf8c062f5ec
Wenxin Tian <wxtian@umich.edu>
2023-04-21 15:59:14 -0400

    update_data

--- preprocess.py
+++ preprocess.py
@@ -1,91 +1,129 @@
 import csv
+import pandas as pd
 
-def checkABCD(str):
-    if 'a' in str and not 'b' in str and not 'c' in str and not 'd' in str:
-        return True
-    if not 'a' in str and 'b' in str and not 'c' in str and not 'd' in str:
-        return True
-    if not 'a' in str and not 'b' in str and 'c' in str and not 'd' in str:
-        return True
-    if not 'a' in str and not 'b' in str and not 'c' in str and 'd' in str:
-        return True
+def checkABCD(str, char):
+    if char == 'a':
+        if 'a' in str and not 'b' in str and not 'c' in str and not 'd' in str:
+            return True
+    elif char == 'b':
+        if not 'a' in str and 'b' in str and not 'c' in str and not 'd' in str:
+            return True
+    elif char == 'c':
+        if not 'a' in str and not 'b' in str and 'c' in str and not 'd' in str:
+            return True
+    elif char == 'd':
+        if not 'a' in str and not 'b' in str and not 'c' in str and 'd' in str:
+            return True
     return False
 
-def process_select_data():
-    with open('ChicagoFSWild.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        # read first row only
-        column_name = next(reader)
-        data = []
-        data.append(column_name)
-        for row in reader:
-            if int(row[4]) <= 8 and checkABCD(row[8]):
-                data.append(row)
-    print(len(data))
+def select_trian_data():
+    data = []
+    select = ['a','b','c','d']
+    first = True
+    # Select Train data 140 in total, 35 each for one label
+    for char in select:
+        with open('ChicagoFSWild.csv', newline='') as input_file:
+            reader = csv.reader(input_file)
+            column_name = next(reader)
+            if first:
+                column_name.append("Label")
+                data.append(column_name)
+                first = False
+            count = 0
+            for row in reader: 
+                # in total 160 data, 40 each
+                if count == 35:
+                    break
+                elif int(row[4]) <= 15 and row[10] == "train" and checkABCD(row[8], char):
+                        count += 1
+                        row.append(char)
+                        data.append(row)
 
-    with open('output.csv', 'w', newline='') as csvfile:
+    with open('output_train.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for row in data:
             writer.writerow(row)
 
-def process_separate_type():
-    a = 0
-    b = 0
-    c = 0
-    d = 0
-    data_train = []
-    data_dev = []
-    data_test = []
-    with open('output.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile,delimiter=';')
-        # read first row only
-        column_name = next(reader)
-        data_train.append(column_name)
-        data_dev.append(column_name)
-        data_test.append(column_name)
-        for row in reader:
-            if row[12] == 'a':
-                a += 1
-            elif row[12] == 'b':
-                b += 1
-            elif row[12] == 'c':
-                c += 1
-            elif row[12] == 'd':
-                d += 1
-            if row[10] == "train":
-                data_train.append(row)
-            if row[10] == "dev":
-                data_dev.append(row)
-            if row[10] == "test":
-                data_test.append(row)
-    print("a: ",a," b: ",b," c: ",c," d: ",d)
+def select_dev_test_data():
+    dev_test = ['dev','test']
+    for name in dev_test:
+        data = []
+        select = ['a','b','c','d']
+        first = True
+        # Select Train data 140 in total, 35 each for one label
+        for char in select:
+            with open('ChicagoFSWild.csv', newline='') as input_file:
+                reader = csv.reader(input_file)
+                column_name = next(reader)
+                if first:
+                    column_name.append("Label")
+                    data.append(column_name)
+                    first = False
+                count = 0
+                for row in reader: 
+                    # in total 30 data, 10 each
+                    if count == 10:
+                        break
+                    elif int(row[4]) <= 15 and row[10] == name and checkABCD(row[8], char):
+                            count += 1
+                            row.append(char)
+                            data.append(row)
+
+        with open('output_{}.csv'.format(name), 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for row in data:
+                writer.writerow(row)
+        
+def combine_output():
+
+    data = []
+    name = ['train', 'dev', 'test']
+    label_counts = {'a': [0, 0, 0], 'b': [0, 0, 0], 'c': [0, 0, 0], 'd': [0, 0, 0]}
+
+    first = True
+    for i, n in enumerate(name):
+        with open(f'output_{n}.csv', newline='') as input_file:
+            reader = csv.reader(input_file)
+            column_name = next(reader)
+            if first:
+                data.append(column_name)
+                first = False
+            for row in reader:
+                data.append(row)
+                label = row[12]
+                if label in label_counts:
+                    label_counts[label][i] += 1
+
+    sum_a = label_counts['a'][0] + label_counts['a'][1] + label_counts['a'][2]
+    sum_b = label_counts['b'][0] + label_counts['b'][1] + label_counts['b'][2]
+    sum_c = label_counts['c'][0] + label_counts['c'][1] + label_counts['c'][2]
+    sum_d = label_counts['d'][0] + label_counts['d'][1] + label_counts['d'][2]
+
+    with open('output_new.csv'.format(name), 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for row in data:
+                writer.writerow(row)
 
 
-    with open('output_train.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        for row in data_train:
-            writer.writerow(row)
-    print("numbe lines train:",len(data_train))
-    
-    with open('output_dev.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        for row in data_dev:
-            writer.writerow(row)
-    print("numbe lines dev:",len(data_dev))
-
-    with open('output_test.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        for row in data_test:
-            writer.writerow(row)
-    print("numbe lines test:",len(data_test))
-
+    print("Summary \n \
+          Label A has total number: {}, with train data: {}, dev data: {}, and test data: {} \n \
+          Label B has total number: {}, with train data: {}, dev data: {}, and test data: {} \n \
+          Label C has total number: {}, with train data: {}, dev data: {}, and test data: {} \n \
+          Label D has total number: {}, with train data: {}, dev data: {}, and test data: {} \
+          ".format(sum_a,label_counts['a'][0],label_counts['a'][1],label_counts['a'][2],
+                   sum_b,label_counts['b'][0],label_counts['b'][1],label_counts['b'][2],
+                   sum_c,label_counts['c'][0],label_counts['c'][1],label_counts['c'][2],
+                   sum_d,label_counts['d'][0],label_counts['d'][1],label_counts['d'][2]))
 
 
 def main():
     # 1. Select subset data from current dataset
-    # process_select_data()
-    # 2. Seperate data to train, dev, and test
-    process_separate_type()
+    # in total, there are 200 datas Train = 200 * 0.7 = 140
+    # Dev = 200 * 0.15 = 30, Test = 200 * 0.15 = 30
+    select_trian_data()
+    select_dev_test_data()
+    combine_output()
+
 
 if __name__ == "__main__":
     main()
