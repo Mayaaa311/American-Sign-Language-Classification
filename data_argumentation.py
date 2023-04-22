81b06f8bb29081b608ca56aa3bbcc2e084b74563
Zhan, Jennifer <ruoyizhan@gmail.com>
2023-04-22 15:45:46 -0400

    edit data_argumentation.py and add flipped and rotated images

--- data_argumentation.py
+++ data_argumentation.py
@@ -4,24 +4,30 @@ import imgaug.augmenters as iaa
 import os
 from PIL import Image
 
-def data_argumentation(directory):
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        input_img = imageio.imread(f)
-        #Horizontal Flip
-        hflip= iaa.Fliplr(p=1.0)
-        input_hf= hflip.augment_image(input_img)
-        new_file_name_flip = f.replace(".png", "_flip.png")
-        imageio.imwrite(new_file_name_flip, input_hf)
-        #Rotation
-        rot1 = iaa.Affine(rotate=(-50,20))
-        input_rot1 = rot1.augment_image(input_img)
-        new_file_name_rot = f.replace(".png", "_rot.png")
-        imageio.imwrite(new_file_name_rot, input_rot1)
+def flip_and_Rotation(directory):
+    # reference: https://www.analyticsvidhya.com/blog/2022/04/image-augmentation-using-3-python-libraries/
+    file_list = os.listdir(directory)
+    for filename in file_list:
+        check_flip_name = filename.replace(".png", "_flip.png")
+        check_rot_name = filename.replace(".png", "_rot.png")
+        if ((check_flip_name not in file_list) and ("flip" not in filename) and 
+            (check_rot_name not in file_list) and ("rot" not in filename)):
+            f = os.path.join(directory, filename)
+            input_img = imageio.imread(f)
+            #Horizontal Flip
+            hflip= iaa.Fliplr(p=1.0)
+            input_hf= hflip.augment_image(input_img)
+            new_file_name_flip = f.replace(".png", "_flip.png")
+            imageio.imwrite(new_file_name_flip, input_hf)
+            #Rotation
+            rot1 = iaa.Affine(rotate=(-50,20))
+            input_rot1 = rot1.augment_image(input_img)
+            new_file_name_rot = f.replace(".png", "_rot.png")
+            imageio.imwrite(new_file_name_rot, input_rot1)
 
 def main():
-    # data argumentation
-    data_argumentation("avg_train")
+    # generate flipped and rotated images
+    flip_and_Rotation("avg_train")
 
 if __name__ == "__main__":
     main()
